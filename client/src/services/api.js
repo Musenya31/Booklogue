a0f654a5049f4f332af7f10c25b652ab4a0a8473
Musenya31 <maryannemusengya@gmail.com>
2025-10-30 11:03:56 +0300

    cors again

--- client/src/services/api.js
+++ client/src/services/api.js
@@ -5,7 +5,7 @@ import axios from 'axios';
 const BASE_URL = import.meta.env.VITE_API_URL || 'http://localhost:5000';
 
 const API = axios.create({
-  baseURL: `${BASE_URL}/api`, // Add /api here for API routes
+  baseURL: `${BASE_URL}api`, // Add /api here for API routes
   headers: {
     'Content-Type': 'application/json',
   },
